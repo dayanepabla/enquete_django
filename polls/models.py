1d83a3e5f146d63dff1ae2a5ad68f5d2bc213bb7
Dayane Pabla <dayanepabla@gmail.com>
2014-10-07 18:15:47 -0300

    Configurações de admin

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
@@ -21,4 +22,8 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
